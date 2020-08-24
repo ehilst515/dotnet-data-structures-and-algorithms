93e430942c6d0b4a2bd920fcb8e2cce1a04efb57
Enrique Hilst <enrique.hilst@gmail.com>
2020-08-24 15:42:57 -0500

    updated wb image

--- Challenges/Program.cs
+++ Challenges/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using Challenges.Arrays;
 
 namespace Challenges
 {
@@ -6,7 +7,9 @@ namespace Challenges
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] arrayToReverse = new[] { 1, 2, 3, 4 };
+            Reverse.ReverseArray(arrayToReverse);
+            Console.WriteLine(String.Join(", ", arrayToReverse));
         }
     }
 }
