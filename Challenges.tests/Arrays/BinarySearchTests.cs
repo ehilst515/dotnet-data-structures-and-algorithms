aca1a65a9a3e1899e82762f61e59364e855a11df
Enrique Hilst <enrique.hilst@gmail.com>
2020-08-26 15:20:14 -0500

    added tests, all passing

--- Challenges.tests/Arrays/BinarySearchTests.cs
+++ Challenges.tests/Arrays/BinarySearchTests.cs
@@ -9,9 +9,11 @@ namespace Challenges.tests.Arrays
     {
         [Theory]
         [InlineData(new[] { 1, 2, 3, 4, 5, 6}, 2, 1)]
-        [InlineData(new[] { 1, 2, 3, 4, 5, 6, 7}, 3, 2)]
+        [InlineData(new[] { 4, 8, 15, 16, 23, 42 }, 15, 2)]
+        [InlineData(new[] { 1, 2, 3, 4, 5, 6, 7 }, 6, 5)]
+        [InlineData(new[] { 11, 22, 33, 44, 55, 66, 77 }, 90, -1)]
+        [InlineData(new[] { 1 }, 1, 0)]
 
-        ›
         public void BinarySearch_returns_n_location(int[] testArray, int testN, int expected)
         {
             // Arrange
