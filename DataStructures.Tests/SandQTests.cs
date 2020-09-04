3d7531fa8ef21cff4d27954ff1fb0527b198d2c4
Enrique Hilst <enrique.hilst@gmail.com>
2020-09-04 17:33:39 -0500

    Node and all of Stack compelte

---
+++ DataStructures.Tests/SandQTests.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using DataStructures.StacksAndQueues;
+using Xunit;
+
+
+namespace DataStructures.Tests
+{
+    public class SandQTests
+    {
+        [Fact]
+        public void IsEmpty_returns_true_if_stack_is_empty()
+        {
+            // Arrange 
+             Stack<int> stack = new Stack<int>();
+
+            // Act
+            bool result = stack.IsEmpty();
+
+            // Assert
+            Assert.True(result);
+
+        }
+
+        [Fact]
+        public void IsEmpty_returns_false_if_stack_is_not_empty()
+        {
+            // Arrange 
+            Stack<int> stack = new Stack<int>();
+            stack.Push(1);
+
+            // Act
+            bool result = stack.IsEmpty();
+
+            // Assert
+            Assert.False(result);
+
+        }
+
+        [Fact]
+        public void Push_adds_to_stack()
+        {
+            // Arrange
+            Stack<int> stack = new Stack<int>();
+
+            // Act
+            stack.Push(3);
+            stack.Push(2);
+            stack.Push(1);
+
+            // Assert
+            Assert.Equal(1, stack.Peek());
+        }
+
+        [Fact]
+        public void Pop_removes_top_of_stack_and_returns_value()
+        {
+            // Arrange
+            Stack<int> stack = new Stack<int>();
+            stack.Push(3);
+            stack.Push(2);
+            stack.Push(1);
+
+            //Act
+            int result = stack.Pop();
+
+            //Assert
+            Assert.Equal(2, stack.Peek());
+            Assert.Equal(1, result);
+
+
+        }
+
+    }
+}
---
+++ DataStructures/StacksAndQueues/Stacking.cs
@@ -0,0 +1,59 @@
+﻿using System;
+namespace DataStructures.StacksAndQueues
+{
+    public class Stack<T>
+    {
+        public class Node
+        {
+
+            public Node(T value)
+            {
+                Value = value;
+            }
+
+            // Value of this node
+            public T Value { get; set; }
+
+            // Pointer to the next node in the list
+            public Node Next { get; set; }
+        }
+
+        public Node Top { get; set; }
+
+        public void Push(T value)
+        {
+            Node node = new Node(value);
+            node.Next = Top;
+            Top = node;
+        }
+
+        public T Pop()
+        {
+            Node temp = Top;
+            Top = temp.Next;
+            temp.Next = null;
+            return temp.Value;
+        }
+
+        public T Peek()
+        {
+            if(Top == null)
+            {
+                throw new InvalidOperationException();
+            }
+            else
+                return Top.Value;
+        }
+
+        public bool IsEmpty()
+        {
+            if (Top == null)
+            {
+                return true;
+            }
+
+            else
+                return false;
+        }
+    }
+}
