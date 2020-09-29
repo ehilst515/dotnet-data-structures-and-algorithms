d27986ed6142a04f13945ed507fb9a8c0cca6afd
Enrique Hilst <enrique.hilst@gmail.com>
2020-09-29 01:11:17 -0500

    Insertion Sort complete

---
+++ Challenges.tests/Arrays/SortTests.cs
@@ -0,0 +1,32 @@
+﻿using Challenges.Sort;
+using Xunit;
+
+
+namespace Challenges.tests.Arrays
+{
+    public class SortTests
+    {
+        public class InsertionTest
+        {
+            [Theory]
+            [InlineData(new[] { 23, 75, 101, 4 }, new int[] { 4, 23, 75, 101 })]
+            [InlineData(new[] { 20, 18, 12, 8, 5, -2 }, new int[] { -2, 5, 8, 12, 18, 20 })]
+            [InlineData(new[] { 5, 12, 7, 5, 5, 7 }, new int[] { 5, 5, 5, 7, 7, 12 })]
+            [InlineData(new[] { 2, 3, 5, 7, 13, 11 }, new int[] { 2, 3, 5, 7, 11, 13 })]
+            public void Insertion_array_in_order(int[] newArray, int[] expected)
+            {
+                // Arrange
+                // InlineData
+
+                // Act
+                int[] result = InsertionSort.Insertion(newArray);
+
+                //Assert
+                Assert.Equal(expected, result);
+            }
+
+
+        }
+
+    }
+}
---
+++ Challenges/Sort/InsertionSort.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Challenges.Sort
+{
+    public class InsertionSort
+    {
+        public static int[] Insertion(int[] array)
+        {
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                for (int j = i + 1; j > 0; j--)
+                {
+                    if (array[j - 1] > array[j])
+                    {
+                        int temp = array[j - 1];
+                        array[j - 1] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
+            return array;
+        }
+    }
+ 
+}
+
