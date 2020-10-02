54c4816381544cc851ad9b462959aff6b6d632ba
Enrique Hilst <enrique.hilst@gmail.com>
2020-10-02 18:01:30 -0500

    GetHash returning hashes, working on Add and Find

---
+++ DataStructures.Tests/HashTest.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using DataStructures.HashTable;
+using System.Linq;
+
+namespace DataStructures.Tests
+{
+    public class HashTest
+    {
+        [Fact]
+        public void GetHash_returns_hash_number()
+        {
+            // Arrange and Act
+            int result = HashTable.HashTable<ValueType>.GetHash("Hi");
+
+            // Assert
+            int expected = 1239;
+            Assert.Equal(expected, result);
+        }
+
+        [Fact]
+        public void Add_adds_to_hash_table()
+        {
+            // Arrange
+            LinkedList<string>[] HT = new LinkedList<string>[10];
+            string value = "Hi";
+
+            // Act
+            HashTable<string>.Add("1239", value , HT);
+
+            // Assert
+            string expected = HashTable<string>.HashFind("1239", HT);
+            Assert.Equal(expected, value);
+        }
+
+
+
+    }
+}
---
+++ DataStructures/HashTable/HashTable.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataStructures.HashTable
+{
+    public class HashTable<TValue>
+    {
+        public static int GetHash(string key)
+        {
+            int[] keyNumArr = new int[key.Length];
+
+            for (int i = 0; i < key.Length; i++)
+                keyNumArr[i] = (int)key[i];
+
+            int sum = 0;
+            foreach (int n in keyNumArr)
+                sum += n;
+
+            int prime = 7;
+ 
+
+            int hashNum = sum * prime;
+
+            return hashNum;       
+        }
+
+        public static void Add(string key, TValue value, LinkedList<TValue>[] HT)
+        {
+            int bucketNum = GetHash(key) % HT.Length;
+            var bucket = HT[bucketNum];
+
+            if (bucket == null)
+            {
+                LinkedList<TValue> newLinkedList = new LinkedList<TValue>();
+                newLinkedList.AddFirst(value);
+            }
+
+            else
+                bucket.AddFirst(value);
+        }
+
+        public static string HashFind(string key, LinkedList<string>[] HT)
+        {
+            int hashNumber = GetHash(key);
+            int bucketNumber = hashNumber % HT.Length;
+
+
+            if (HT[bucketNumber] == null)
+                return null;
+
+            else
+                return HT[bucketNumber].First();
+
+
+        }
+
+        public class Node<T>
+        {
+            public Node(T value, string key)
+            {
+                Value = value;
+                Key = key;
+            }
+
+            // Hash Key
+            public string Key { get; set; }
+            // Value of this node
+            public T Value { get; set; }
+            // Pointer to the next node in the list
+            public Node<T> Next { get; set; }
+
+        }
+    }
+      
+}
