f6ef05b0ce1fc552419fc7dcd3abf2844b74ff80
Enrique Hilst <enrique.hilst@gmail.com>
2020-10-06 15:37:37 -0500

    Tree instersection challenge compelte

---
+++ DataStructures/Trees/TreeInstersction.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures.Trees
+{
+    public class TreeIntersection<T>
+    {
+        public static Dictionary<int, int> hashTable = new Dictionary<int, int>();
+
+        public static List<int> TreeIntersect(BinaryTree<int> tree1, BinaryTree<int> tree2)
+        {
+            List<int> resultList = new List<int>();
+
+            var list = tree1.Breadth_First();
+
+            foreach (int num in list)
+                hashTable.Add(num, num);
+             
+            var list2 = tree2.Breadth_First();
+
+            foreach (int num in list2)
+            {
+                if (hashTable.ContainsKey(num))
+                    resultList.Add(num);
+
+                else
+                    hashTable.Add(num, num);
+            }
+
+            return resultList;
+        }
+    }
+}
