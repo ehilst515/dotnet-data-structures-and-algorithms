bdf95a8438469ea0cea2b7d597ccf9e5219d7b90
Enrique Hilst <enrique.hilst@gmail.com>
2020-11-18 16:36:38 -0600

    Added First Dupe and First Non Rep Char challanges

---
+++ Challenges.tests/Arrays/FirstDupeTest.cs
@@ -0,0 +1,25 @@
+﻿using Challenges.Arrays;
+using Xunit;
+
+
+namespace Challenges.tests.Arrays
+{
+     public class FirstDupeTest
+    {
+        [Theory]
+        [InlineData(new[] { 1, 2, 3, 4, 1, 5 }, 1)]
+        [InlineData(new[] { 1, 2, 3, 4, 5, 6 }, -1)]
+        [InlineData(new[] { 1, 6, 2, 3, 4, 5, 6 }, 6)]
+        [InlineData(new[] { 2, 1, 3, 5, 3, 2 }, 3)]
+        public void FirstDupeTest_returns_first_dupe(int[] array, int expected)
+        {
+            // Arrange InlineData
+
+            //Act
+            int result = FirstDupelicate.firstDupe(array);
+
+            //Assert
+            Assert.Equal(expected, result);
+        }
+    }
+}
---
+++ Challenges.tests/Strings/StringTests.cs
@@ -0,0 +1,27 @@
+﻿using Challenges.Strings;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Challenges.tests.Strings
+{
+    public class StringTests
+    {
+        [Theory]
+        [InlineData("abacabad", "c")]
+        [InlineData("abaababb", "_")]
+
+        public void firstNotRepeatingCharacter_returns_correctly(string s, string exp)
+        {
+            //Arrange
+            //InlineData
+
+            //Act
+            var actual = firstNonRepeatChar.firstNotRepeatingCharacter(s);
+
+            //Assert
+            Assert.Equal(exp, actual.ToString());
+        }
+    }
+}
