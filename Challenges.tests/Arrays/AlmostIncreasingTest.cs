5b8d158379bf4bdc6fdabeb7dd69f4cd39c105e3
Enrique Hilst <enrique.hilst@gmail.com>
2020-11-25 09:18:29 -0600

    Added almost increasing

---
+++ Challenges.tests/Arrays/AlmostIncreasingTest.cs
@@ -0,0 +1,60 @@
+﻿using Challenges.Arrays;
+using Xunit;
+
+namespace Challenges.tests.Arrays
+{
+    public class AlmostIncreasingTest
+    {
+        [Fact]
+        public void IncreasingCheck_Returns_false_with_doubles()
+        {
+            // Arrange
+            int[] s = new int[] { 1, 1, 2, 3, 4, 4 };
+
+            // Act 
+            bool actual = AlmostIncreasingSequence.IncreasingCheck(s);
+
+            // Assert
+            Assert.False(actual);
+        }
+
+        [Fact]
+        public void IncreasingCheck_Returns_false_with_pattern()
+        {
+            // Arrange
+            int[] s = new int[] { 1, 2, 1, 2 };
+
+            // Act 
+            bool actual = AlmostIncreasingSequence.IncreasingCheck(s);
+
+            // Assert
+            Assert.False(actual);
+        }
+
+        [Fact]
+        public void IncreasingCheck_Returns_true_properly()
+        {
+            // Arrange
+            int[] s = new int[] { 10, 1, 2, 3, 4, 5 };
+
+            // Act 
+            bool actual = AlmostIncreasingSequence.IncreasingCheck(s);
+
+            // Assert
+            Assert.True(actual);
+        }
+
+        [Fact]
+        public void IncreasingCheck_Returns_false_with_repeat_and_less_than()
+        {
+            // Arrange
+            int[] s = new int[] { 1, 2, 3, 4, 5, 3, 5, 6 };
+
+            // Act 
+            bool actual = AlmostIncreasingSequence.IncreasingCheck(s);
+
+            // Assert
+            Assert.False(actual);
+        }
+    }
+}
---
+++ Challenges/Arrays/AlmostIncreasingSequence.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Challenges.tests.Arrays
+{
+    public class AlmostIncreasingSequence
+    {
+        public static bool IncreasingCheck(int[] s)
+        {
+            if (s.Length == 3 && s[0] <= s[2])
+                return true;
+
+            if (s.Length == 2 && s[0] <= s[1])
+                return true;
+
+            if (s.Length == 3 && s[0] > s[2])
+                return false;
+
+            if (s.Length == 2 && s[0] > s[1])
+                return false;
+
+            int removeCount = 1;
+
+            for(int i = 2; i < s.Length + 1; i++)
+            {
+                if(removeCount == 0)
+                {
+                    if (s[i - 3] == s[i - 1])
+                        return false;
+
+                    for(i = i - 2; i < s.Length - 1; i++)
+                        if (s[i] >= s[i + 1])
+                            return false;
+
+                    return true;
+                }
+
+                if (removeCount == 1)
+                    if (s[i - 2] >= s[i - 1] || s[i - 2] == s[i])
+                        removeCount = 0;
+
+                if(removeCount == 1)
+                {
+
+                    if (s[i - 2] >= s[i - 1] && s[i - 2] >= s[i])
+                        return false;
+
+                    if ((s[i - 2] > s[i - 1] && s[i - 2] < s[i]) || s[i - 2] == s[i - 1])
+                        removeCount = 0;
+                }
+            }
+
+            return true;
+        }
+    }
+}
